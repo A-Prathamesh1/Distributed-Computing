d7589b9599a4ed709ad15e9ae2d6fa0fb1d44f9e
A-Prathamesh1 <33331374+A-Prathamesh1@users.noreply.github.com>
2018-04-06 21:08:46 -0700

    Update Multiplication_Server.py

--- Multiplication_Server.py
+++ Multiplication_Server.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python
 """Multiplication_Server.py : This file performs multiplication"""
-__author__ = "Priyanka Nalawade Net Id: vu2628 & Prathamesh Ausekar Net ID:eq7948"
+__author__ = "Prathamesh Ausekar Net ID:eq7948"
 
 import socket
 import json
